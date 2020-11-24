db68d16debaf431a06326757159c1f4235ff0dd0
Blue <62485293+kamelzarandah@users.noreply.github.com>
2020-11-24 13:14:56 -0800

    Add files via upload

---
+++ weapon_log/Config.lua
@@ -0,0 +1,3 @@
+Config = { }
+Config.combatlog = true -- change to false if u dont want combat logging to be recorded
+Config.webhook = "your web hook here "
---
+++ weapon_log/Server/Server.lua
@@ -0,0 +1,23 @@
+local dead 
+RegisterServerEvent("DiscordBot:playerDied")
+AddEventHandler("DiscordBot:playerDied", function(msg,Weapon)
+    local webhook = Config.webhook
+    local message
+    dead = 1
+    if Weapon ~= nil then
+         message = msg.." Weapon: "..Weapon
+    else
+         message = msg
+    end
+    SendWebhookMessage(webhook,message)
+end)
+function SendWebhookMessage(webhook,message)
+    PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({content = message}), { ['Content-Type'] = 'application/json' })
+end 
+AddEventHandler('playerDropped', function(reason) 
+    if dead == 1 and Config.combatlog then
+        local webhook = Config.webhook
+        message = GetPlayerName(source) .. " Combat logged"
+        SendWebhookMessage(webhook,message)
+    end
+end)

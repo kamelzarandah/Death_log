04b434482233e94eaac0111712051e7a3f50dde1
Blue <62485293+kamelzarandah@users.noreply.github.com>
2020-11-25 08:16:30 -0800

    Update Server.lua

--- weapon_log/Server/Server.lua
+++ weapon_log/Server/Server.lua
@@ -1,9 +1,12 @@
-local dead 
+
+TriggerEvent("getCore",function(core)
+    VorpCore = core
+end)
+VORP = exports.vorp_core:vorpAPI()
 RegisterServerEvent("DiscordBot:playerDied")
 AddEventHandler("DiscordBot:playerDied", function(msg,Weapon)
     local webhook = Config.webhook
     local message
-    dead = 1
     if Weapon ~= nil then
          message = msg.." Weapon: "..Weapon
     else
@@ -14,11 +17,14 @@ end)
 function SendWebhookMessage(webhook,message)
     PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({content = message}), { ['Content-Type'] = 'application/json' })
 end 
-AddEventHandler('playerDropped', function(reason) 
-    if dead == 1 and Config.combatlog then
+AddEventHandler('playerDropped', function(reason)
+    local User = VorpCore.getUser(source)
+    local Character = User.getUsedCharacter
+    local isdead = Character.isdead  
+    if isdead and Config.combatlog then
         local webhook = Config.webhook
         message = GetPlayerName(source) .. " Combat logged"
         SendWebhookMessage(webhook,message)
-        dead = nil
+        isdead = nil
     end
 end)
